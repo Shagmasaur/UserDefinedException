2eddcb21e5cc7fb8621362627d8e54200a3d768e
abhishek.lashley <abhishek.lashley@gmail.com>
2022-08-25 11:29:15 +0530

    myfirstException  done

--- src/PracticeQuestion/MyMain.java
+++ src/PracticeQuestion/MyMain.java
@@ -3,37 +3,32 @@ package PracticeQuestion;
 import java.util.Scanner;
 
 public class MyMain {
-    public int a;
-    public int b;
 
-
-    public void add(int a, int b) throws MyFirstException
+    public void add(int firstNumber, int secondNumber) throws MyFirstException
     {
-        int sum=a+b;
+        int sum=firstNumber+secondNumber;
         if(sum<150)
         {
-            throw new MyFirstException("Custom exception occurred");
+            throw new MyFirstException("Custom Exception Occurred");
         }
         System.out.println("Sum of two numbers is : " +sum);
 
     }
-
     public static void main(String[] args)
     {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter first number : ");
-        int a= scanner.nextInt();
+        int firstNumber= scanner.nextInt();
         System.out.println("Enter second number : ");
-        int b= scanner.nextInt();
+        int secondNumber= scanner.nextInt();
 
         MyMain random = new MyMain();
         try {
-            random.add(a,b);
+            random.add(firstNumber,secondNumber);
         }
         catch (MyFirstException e)
         {
             System.out.println(e);
         }
     }
-
 }
